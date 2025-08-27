c8359d462c1fa5d834389c62c106e1b5e83b1ee0
LucasInsa07 <167370802+LucasInsa07@users.noreply.github.com>
2025-08-27 02:26:08 -0300

    Renombrar contrasena y agregar administrador

---
+++ src/main/java/com/demo/sistema_gestion_escolar/repository/AdministradorRepository.java
@@ -0,0 +1,13 @@
+package com.demo.sistema_gestion_escolar.repository;
+
+import com.demo.sistema_gestion_escolar.model.Administrador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
+    Optional<Administrador> findByUsuario(String usuario);
+}
+
